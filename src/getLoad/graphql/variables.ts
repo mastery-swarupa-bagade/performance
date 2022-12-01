c3a3cd4a2a2887069888b2ce263e08ed4aa9cfa7
mastery-swarupa-bagade <ext.swarupa.bagade@matery.net>
2022-12-01 13:36:58 +0530

    var

--- src/getLoad/graphql/variables.ts
+++ src/getLoad/graphql/variables.ts
@@ -1,7 +1,7 @@
 import {SharedArray} from 'k6/data'
 
 const dataPath = '../../getLoad/data/loadId.json'
-const data = new SharedArray ('variables', function (){
+const data = new SharedArray('variables', function () {
   return JSON.parse(open(dataPath))
 }) 
 
